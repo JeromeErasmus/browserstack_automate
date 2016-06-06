6e78ad62a839999774bf308ab9d922eeb9f4d1ee
jerome <jerome.erasmus@gmail.com>
2016-06-06 23:51:21 +1000

    added modal dialog controllers

--- automate/static/js/app.js
+++ automate/static/js/app.js
@@ -1,5 +1,5 @@
-
-var app = angular.module("myApp", ['ngResource', 'ui.bootstrap']);
+/*
+var app = angular.module("myApp", ['ngResource']);
 
 app.config(['$interpolateProvider', function($interpolateProvider) {
   $interpolateProvider.startSymbol('{[');
@@ -22,32 +22,34 @@ app.factory("RequestResults", function($resource) {
 app.factory("Project", function($resource) {
   return $resource("/project")
 });
+*/
+var app = angular.module('myApp', ['ngAnimate', 'ui.bootstrap', 'ngResource']);
 
-app.factory("Modal", function($scope, $modal, $log) {
-  
-  $scope.items = ['item1', 'item2', 'item3'];
+app.config(['$interpolateProvider', function($interpolateProvider) {
+  $interpolateProvider.startSymbol('{[');
+  $interpolateProvider.endSymbol(']}');
 
-  $scope.open = function (size) {
-    var modalInstance;
-    var modalScope = $scope.$new();
-    modalScope.ok = function () {
-            modalInstance.close(modalScope.selected);
-    };
-    modalScope.cancel = function () {
-            modalInstance.dismiss('cancel');
-    };      
-    
-    modalInstance = $modal.open({
-      template: '<my-modal></my-modal>',
-      size: size,
-      scope: modalScope
-      }
-    );
+}]);
 
-    modalInstance.result.then(function (selectedItem) {
-      $scope.selected = selectedItem;
-    }, function () {
-      $log.info('Modal dismissed at: ' + new Date());
-    });
-  };
+
+app.config(['$interpolateProvider', function($interpolateProvider) {
+  $interpolateProvider.startSymbol('{[');
+  $interpolateProvider.endSymbol(']}');
+
+}]);
+
+app.factory("RequestRunners", function($resource) {
+  return $resource("/data/runners", {}, {
+    query: { isArray: false }
+  })
+});
+
+app.factory("RequestResults", function($resource) {
+  return $resource("/data/results/:id", {id:1}, {
+    query: { isArray: false }
+  })
+});
+
+app.factory("Project", function($resource) {
+  return $resource("/project")
 });
--- automate/static/js/controllers/controller.js
+++ automate/static/js/controllers/controller.js
@@ -1,13 +1,14 @@
+
 app.controller("ResourceController", function($scope, RequestRunners, RequestResults, Project) {
-  /*********************************************
-  Varz 
-  **********************************************/
+  // --------------------------------------------
+  // Varz 
+  // --------------------------------------------
   $scope.runners = [];
 
 
-  /*********************************************
-  Project 
-  **********************************************/
+  // --------------------------------------------
+  // Project 
+  // --------------------------------------------
   //$scope.project = new Project(); 
   //$scope.project.project_path = "asdasd";
 
@@ -36,9 +37,9 @@ app.controller("ResourceController", function($scope, RequestRunners, RequestRes
     });
   };
 
-  /*********************************************
-  Runners
-  **********************************************/
+  // --------------------------------------------
+  // Runners
+  // --------------------------------------------
   $scope.getRunners = function()
   {
     RequestRunners.query(function(data) {
@@ -65,41 +66,75 @@ app.controller("ResourceController", function($scope, RequestRunners, RequestRes
 
  
 
-  /*********************************************
-  Modal Controls
-  **********************************************/
+  // --------------------------------------------
+  // Modal Controls
+  // --------------------------------------------
 
   //$scope.createProject('C:\\Users\\administrator\\AppData\\Roaming');
  // $scope.createProject('C:\\Users\\administrator\\AppData\\Roaming');
   
 
 
-/*  var project = Project.get({ id: $scope.id }, function() {
-    console.log(project);
-    console.log("ASDASDD");
-  }); // get() returns a single project*/
+// var project = Project.get({ id: $scope.id }, function() {
+//    console.log(project);
+//    console.log("ASDASDD");
+//  }); // get() returns a single projec
   
-/*
-  var project = Project.query(function() {
-    console.log(project);
-  }); //query() returns all the project
-*/
-  
+//  var project = Project.query(function() {
+//    console.log(project);
+//  }); //query() returns all the project
 
 
 });
-app.directive('myModal', function() {
-    return {
-        restrict: 'E',
-        templateUrl: 'myModalContent.html',
-        controller: function ($scope) {
-          $scope.selected = {
-            item: $scope.items[0] 
-          };
+
+app.controller('ModalDemoCtrl', function($scope, $uibModal, $log) {
+
+  $scope.items = ['item1', 'item2', 'item3'];
+
+  $scope.animationsEnabled = true;
+
+  $scope.open = function (size) {
+
+    var modalInstance = $uibModal.open({
+      animation: $scope.animationsEnabled,
+      templateUrl: 'myModalContent.html',
+      controller: 'ModalInstanceCtrl',
+      size: size,
+      resolve: {
+        items: function () {
+          return $scope.items;
         }
-    };
-});
-app.controller('ModalDemoCtrl', function($scope, Modal) {
+      }
+    });
 
-  
+    modalInstance.result.then(function(selectedItem) {
+      $scope.selected = selectedItem;
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+
+  $scope.toggleAnimation = function () {
+    $scope.animationsEnabled = !$scope.animationsEnabled;
+  };
+
+});
+
+// Please note that $uibModalInstance represents a modal window (instance) dependency.
+// It is not the same as the $uibModal service used above.
+
+app.controller('ModalInstanceCtrl', function ($scope, $uibModalInstance, items) {
+
+  $scope.items = items;
+  $scope.selected = {
+    item: $scope.items[0]
+  };
+
+  $scope.ok = function () {
+    $uibModalInstance.close($scope.selected.item);
+  };
+
+  $scope.cancel = function () {
+    $uibModalInstance.dismiss('cancel');
+  };
 });
